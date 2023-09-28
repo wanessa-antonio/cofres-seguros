91bf12144318ec45bfec5a87ff9bbb7894501f8a
Wanessa <wanessa.ant04@gmail.com>
2023-09-28 08:24:31 -0300

    commit add.

--- cofres-seguros/README.md
+++ cofres-seguros/README.md
@@ -6,13 +6,10 @@ Codifique o seguinte cenário.
 
 Você foi contratado para desenvolver um sistema de cofres seguros. O objetivo é criar dois tipos de cofres: um Cofre Digital, que é aberto por senha, e um Cofre Físico, que é aberto por chave.
 
-Entrada:
+
 - O programa permitirá ao usuário digitar o tipo de cofre a ser criado: "digital" ou "físico". 
 - Se for escolhido o tipo "digital", o usuário deve digitar a senha e a confirmação de senha do cofre digital (ambas com apenas números). 
 - Caso seja escolhido o tipo "físico", não é necessário nenhuma outra entrada.
-
-Saída:
-- O programa irá exibir as informações do cofre criado. Para o Cofre Digital, será exibido o tipo e o método de abertura (senha). Para o Cofre Físico, será exibido apenas o tipo e o método de abertura (chave).
 
 Exemplos:
 
@@ -33,20 +30,7 @@ Metodo de abertura: Senha
 Senha incorreta!
 
 
-
 2. Entrada:
-
-fisico
-
-2. Saída:
-
-Tipo: Cofre Fisico
-
-Metodo de abertura: Chave
-
-
-
-3. Entrada:
 
 digital
 
@@ -54,7 +38,7 @@ digital
 
 2525
 
-3. Saída:
+2. Saída:
 
 Tipo: Cofre Digital
 
--- cofres-seguros/src/Desafio.java
+++ cofres-seguros/src/Desafio.java
@@ -1,67 +1,79 @@
 import java.util.Scanner;
 
 abstract class Cofre {
-  protected String tipo;
-  protected String metodoAbertura;
+    protected String tipo;
+    protected String metodoAbertura;
 
-  public Cofre(String tipo, String metodoAbertura) {
-    this.tipo = tipo;
-    this.metodoAbertura = metodoAbertura;
-  }
+    public Cofre(String tipo, String metodoAbertura) {
+        this.tipo = tipo;
+        this.metodoAbertura = metodoAbertura;
+    }
 
-  public void imprimirInformacoes() {
-    System.out.println("Tipo: " + this.tipo);
-    System.out.println("Metodo de abertura: " + this.metodoAbertura);
-  }
+    public void imprimirInformacoes() {
+        System.out.println("Tipo: " + this.tipo);
+        System.out.println("Método de abertura: " + this.metodoAbertura);
+    }
 }
 
 class CofreDigital extends Cofre {
 
-  private int senha;
+    private int senha;
 
-  public CofreDigital(int senha) {
-    super("Cofre Digital", "Senha");
-    this.senha = senha;
-  }
+    public CofreDigital(int senha) {
+        super("Cofre Digital", "Senha");
+        this.senha = senha;
+    }
 
-  public boolean validarSenha(int confirmacaoSenha) {
-    return confirmacaoSenha == this.senha;
-  }
+    public boolean validarSenha(int confirmacaoSenha) {
+        return confirmacaoSenha == this.senha;
+    }
 }
 
 class CofreFisico extends Cofre {
 
-  public CofreFisico() {
-    super("Cofre Fisico", "Chave");
-  }
-
+    public CofreFisico() {
+        super("Cofre Físico", "Chave");
+    }
 }
 
 public class Desafio {
-  public static void main(String[] args) {
-    try (// Lê o tipo de cofre (primeira linha da entrada)
-    Scanner scanner = new Scanner(System.in)) {
-      String tipoCofre = scanner.nextLine();
-      
-      //condição necessária para a verificação dos cofres seguros:
-      if (tipoCofre.equalsIgnoreCase("digital")) {
-        int senha = scanner.nextInt();
-        int confirmacaoSenha = scanner.nextInt();
-        
-        CofreDigital cofreDigital = new CofreDigital(senha);
-        
-        System.out.println("Tipo: Cofre Digital");
-        System.out.println("Metodo de abertura: Senha");
-        
-        if (cofreDigital.validarSenha(confirmacaoSenha)) {
-          System.out.println("Cofre aberto!");
-        } else {
-          System.out.println("Senha incorreta!");
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Solicitando ao usuário que digite o tipo de cofre (digital ou físico)
+            System.out.print("Por favor, digite o tipo de cofre (digital ou físico): ");
+            String tipoCofre = scanner.nextLine();
+
+            System.out.println();
+
+            // Condição necessária para a verificação dos cofres seguros:
+            if (tipoCofre.equalsIgnoreCase("digital")) {
+                // Solicitando ao usuário que digite a senha
+                System.out.print("Por favor, digite a senha: ");
+                int senha = scanner.nextInt();
+
+                System.out.println();
+                
+                // Solicitando ao usuário que confirme a senha
+                System.out.print("Por favor, confirme a senha: ");
+                int confirmacaoSenha = scanner.nextInt();
+
+                System.out.println();
+
+                CofreDigital cofreDigital = new CofreDigital(senha);
+
+                System.out.println("Tipo: Cofre Digital");
+                System.out.println("Método de abertura: Senha");
+
+                if (cofreDigital.validarSenha(confirmacaoSenha)) {
+                    System.out.println("Cofre aberto!");
+                } else {
+                    System.out.println("Senha incorreta!");
+                }
+            } else if (tipoCofre.equalsIgnoreCase("físico")) {
+                CofreFisico cofreFisico = new CofreFisico();
+                cofreFisico.imprimirInformacoes();
+            }
         }
-      } else if (tipoCofre.equalsIgnoreCase("fisico")) {
-        CofreFisico cofreFisico = new CofreFisico();
-        cofreFisico.imprimirInformacoes();
-      }
     }
-  }
 }
